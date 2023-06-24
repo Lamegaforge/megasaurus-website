9716559793a8dc18f0cf2482221b666a55835263
Clément Galidie <galidie.clement@gmail.com>
2023-06-24 13:12:58 +0200

    Fixed game title color and sections spacing-y

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,23 +20,23 @@
 <body class="h-full antialiased">
     <x-nav />
 
-    <main class="mt-[84px] lg:mt-[68px]">
+    <main class="mt-[84px] lg:mt-[68px] mb-10">
         <section class="relative flex items-center h-[600px] mb-6">
             <x-featured-clip :featuredClip="$featuredClip">
             </x-featured-clip>
         </section>
 
-        <section class="container mx-auto mb-6">
+        <section class="container mx-auto mt-8">
             <x-clip-slider title="Clips récents" sliderSelector="js-latest-clips-slider" :items="$latestAvailableClips">
             </x-clip-slider>
         </section>
 
-        <section class="container mx-auto mb-6">
+        <section class="container mx-auto mt-8">
             <x-game-slider title="Nouveaux jeux" sliderSelector="js-latest-games-slider" :items="$latestGames">
             </x-game-slider>
         </section>
 
-        <section class="container mx-auto mb-6">
+        <section class="container mx-auto mt-8">
             <x-game-slider title="Jeux populaires" sliderSelector="js-popular-games-slider" :items="$popularGames">
             </x-game-slider>
         </section>
