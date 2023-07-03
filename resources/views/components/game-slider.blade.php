f1cd7e935b068fc0bfadf460da1f02bd184b184e
Clément Galidie <galidie.clement@gmail.com>
2023-07-03 23:22:53 +0200

    Fixed slider images sizing

--- resources/views/components/game-slider.blade.php
+++ resources/views/components/game-slider.blade.php
@@ -3,7 +3,7 @@
     <div class="splide__track">
         <ul class="splide__list">
             @foreach($items as $item)
-            <li class="splide__slide !mr-4">
+            <li class="splide__slide">
                 <a href="{{ route('games.show', $item->uuid) }}">
                     <img src="{{ $item->card() }}" alt="">
                 </a>
