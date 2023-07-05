2dbd5edda5deb6c1aa418e569e4d507ae074faa0
Clément Galidie <galidie.clement@gmail.com>
2023-07-06 00:08:26 +0200

    Fixed text styles

--- resources/views/components/featured-clip.blade.php
+++ resources/views/components/featured-clip.blade.php
@@ -8,13 +8,13 @@
         <iframe class="absolute top-0 left-0 w-full h-full" src="{{ $iframeSrc() }}" allowfullscreen></iframe>
 </div>
 @if($hasClipInfos)
-    <div class="relative text-white lg:self-center">
-         {{-- <pre>
+    <div class="relative mt-8 lg:self-center lg:mt-0">
+        {{-- <pre class="relative text-white">
             {{ print_r($featuredClip) }}
-         </pre> --}}
-         <h2>{{ $featuredClip->title }}<h2>
-         <p>par {{ $featuredClip->author->name }}</p>
-         <p>{{ $featuredClip->views }} vues</p>
+        </pre> --}}
+        <h2 class="text-light-shadow mb-4 text-2xl text-white lg:mb-5 lg:text-3xl">{{ $featuredClip->title }}<h2>
+        <p class="text-light-shadow text-white text-lg lg:text-xl">par {{ $featuredClip->author->name }}</p>
+        <p class="text-light-shadow text-white text-lg lg:text-xl">{{ $featuredClip->views }} vues</p>
     </div>
 @endif
 </div>
