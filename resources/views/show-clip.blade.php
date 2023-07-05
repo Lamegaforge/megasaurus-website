2ef07430b13a0f91056575bd4dcdd6252992c8de
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-07-05 08:17:00 +0200

    feat : add clips section on shop.clip page

--- resources/views/show-clip.blade.php
+++ resources/views/show-clip.blade.php
@@ -16,16 +16,20 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 </head>
+
 <body class="h-full antialiased">
     <x-nav />
+    <main class="mt-[84px] lg:mt-[68px] mb-10">
+        <section class="relative flex items-center h-[600px] mb-6">
+            <x-featured-clip :featuredClip="$clip" :infos="true"></x-featured-clip>
+        </section>
 
-    <section class="relative flex items-center h-[600px] mb-6">
-        <x-featured-clip
-            :featuredClip="$clip"
-            :infos="true"
-        ></x-featured-clip>
-    </section>
-
+        <section class="container mx-auto mt-8 lg:px-10">
+            <x-clip-slider title="Autre clips" sliderSelector="js-latest-clips-slider" :items="$randomGameClips">
+            </x-clip-slider>
+        </section>
+    </main>
     <x-footer></x-footer>
 </body>
+
 </html>
