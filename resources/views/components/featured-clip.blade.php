64a105f1c8b0bf3e55c5a4f8097bc28f484703d9
Clément Galidie <galidie.clement@gmail.com>
2023-07-19 23:06:18 +0200

    Fixed video height on medium resolutions

--- resources/views/components/featured-clip.blade.php
+++ resources/views/components/featured-clip.blade.php
@@ -1,5 +1,5 @@
 <div class="absolute inset-x-0 inset-y-0 bg-no-repeat bg-cover blur-sm" style="background-image: url('{{ $featuredClip->thumbnail() }}')"></div>
-<div class="max-w-full mx-auto px-5 @if($infos) wrapper-has-infos w-[1100px] xl:px-0 @else w-[900px] lg:px-0 @endif">
+<div class="max-w-full mx-auto px-5 @if($infos) wrapper-has-infos w-[1100px] mb-4 lg:mb-0 xl:px-0 @else w-[900px] lg:px-0 @endif">
     <div class="relative aspect-video">
         <iframe class="absolute top-0 left-0 w-full h-full" src="{{ $iframeSrc() }}" allowfullscreen></iframe>
     </div>
