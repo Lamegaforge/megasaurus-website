2b0bc580967309c1f5130259b402c1c206a4e13b
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-07-20 20:51:41 +0200

    Changed featured clip design

--- resources/views/components/featured-clip.blade.php
+++ resources/views/components/featured-clip.blade.php
@@ -6,10 +6,10 @@
     @if($infos)
     <div class="relative mt-8 lg:self-center lg:mt-0">
         <h1 class="text-4xl font-extrabold leading-10 text-white">{{ $featuredClip->title }}<h1>
-        <p class="mt-5 text-sm text-gray-300">par {{ $featuredClip->author->name }}</p>
+        <a class="mt-1font-semibold text-white hover:text-indigo-400" href="{{ route('games.show', $featuredClip->game->uuid) }}">{{ $featuredClip->game->name }}</a>
+        <p class="mt-5 text-sm text-gray-300">Par {{ $featuredClip->author->name }}</p>
         <p class="mt-1 text-sm text-gray-300">Publié il y a {{ $featuredClip->publishedAgo() }}</p>
         <p class="mt-1 text-sm text-gray-300">{{ $featuredClip->views }} vues</p>
-        <a class="mt-1font-semibold text-white hover:text-indigo-400" href="{{ route('games.show', $featuredClip->game->uuid) }}">{{ $featuredClip->game->name }}</a>
     </div>
     @endif
 </div>
