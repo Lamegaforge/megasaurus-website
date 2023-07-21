36efbd70845993c85b4849b77135b12f0289cb38
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-07-21 22:29:12 +0200

    added ShowClipControllerTest

---
+++ tests/Feature/ShowClipControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Domain\Models\Clip;
+
+class ShowClipControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function it_can_display_clip_from_uuid(): void
+    {
+        $clip = Clip::factory()->create();
+
+        Clip::factory()->for($clip->game)->count(5)->create();
+
+        $response = $this->get('clips/' . $clip->uuid);
+
+        $response
+            ->assertOk()
+            ->assertSee($clip->title)
+            ->assertSee($clip->game->name);
+
+        $randomGameClips = $response->original->offsetGet('randomGameClips');
+
+        $this->assertCount(6, $randomGameClips);
+    }
+}
