074c1572999690ce74470110f4be03f34b082b63
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-07-29 12:46:00 +0200

    added lazyloading on clip iframe

--- resources/views/components/featured-clip.blade.php
+++ resources/views/components/featured-clip.blade.php
@@ -1,7 +1,7 @@
 <div class="absolute inset-x-0 inset-y-0 bg-no-repeat bg-cover blur" style="background-image: url('{{ $featuredClip->thumbnail() }}')"></div>
 <div class="max-w-full mx-auto px-5 @if($infos) wrapper-has-infos w-[1100px] mb-4 lg:mb-0 xl:px-0 @else w-[900px] lg:px-0 @endif">
     <div class="relative aspect-video">
-        <iframe class="absolute top-0 left-0 w-full h-full" src="{{ $iframeSrc() }}" allowfullscreen></iframe>
+        <iframe loading="lazy" class="absolute top-0 left-0 w-full h-full" src="{{ $iframeSrc() }}" allowfullscreen></iframe>
     </div>
     @if($infos)
     <div class="relative mt-8 lg:self-center lg:mt-0">
