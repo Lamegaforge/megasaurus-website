a365b53aedd558018f69bd7f3ac8f6c5c4e405da
DeGraciaMathieu <degracia.mathieu@gmail.com>
2023-08-05 16:39:03 +0200

    Added common layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,27 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+@extends('layouts.common')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title', 'MEGASAURUS')
 
-    @vite('resources/css/app.scss')
-    @vite('resources/js/app.js')
+@section('main')
+<main class="mt-[84px] lg:mt-[68px] mb-10">
+    <section class="relative flex items-center h-[600px] mb-6">
+        <x-featured-clip :featuredClip="$featuredClip">
+        </x-featured-clip>
+    </section>
 
-    <title>Megasaurus</title>
+    <section class="container mx-auto mt-8 lg:px-10">
+        <x-clip-slider title="Clips récents" sliderSelector="js-latest-clips-slider" :items="$latestAvailableClips">
+        </x-clip-slider>
+    </section>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-</head>
+    <section class="container mx-auto mt-8 lg:px-10">
+        <x-game-slider title="Nouveaux jeux" sliderSelector="js-latest-games-slider" :items="$latestGames">
+        </x-game-slider>
+    </section>
 
-<body class="h-full antialiased">
-    <x-nav />
-
-    <main class="mt-[84px] lg:mt-[68px] mb-10">
-        <section class="relative flex items-center h-[600px] mb-6">
-            <x-featured-clip :featuredClip="$featuredClip">
-            </x-featured-clip>
-        </section>
-
-        <section class="container mx-auto mt-8 lg:px-10">
-            <x-clip-slider title="Clips récents" sliderSelector="js-latest-clips-slider" :items="$latestAvailableClips">
-            </x-clip-slider>
-        </section>
-
-        <section class="container mx-auto mt-8 lg:px-10">
-            <x-game-slider title="Nouveaux jeux" sliderSelector="js-latest-games-slider" :items="$latestGames">
-            </x-game-slider>
-        </section>
-
-        <section class="container mx-auto mt-8 lg:px-10">
-            <x-game-slider title="Jeux populaires" sliderSelector="js-popular-games-slider" :items="$popularGames">
-            </x-game-slider>
-        </section>
-    </main>
-
-    <x-footer></x-footer>
-</body>
-
-</html>
+    <section class="container mx-auto mt-8 lg:px-10">
+        <x-game-slider title="Jeux populaires" sliderSelector="js-popular-games-slider" :items="$popularGames">
+        </x-game-slider>
+    </section>
+</main>
+@endsection
