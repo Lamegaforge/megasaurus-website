af7897016fa0620bdb01e051500174b2386b3114
Clément Galidie <galidie.clement@gmail.com>
2023-08-13 22:58:57 +0200

    Fixed clip's image ratio and wrapper's height

--- resources/views/show-game.blade.php
+++ resources/views/show-game.blade.php
@@ -9,17 +9,17 @@
         :infos="true"
     >
     </x-featured-clip> --}}
-    <section class="relative flex items-center lg:h-[600px]">
+    <section class="relative flex items-center lg:h-[400px]">
         <div
             class="absolute inset-x-0 inset-y-0 bg-no-repeat bg-cover blur"
             style="background-image: url('{{ $gameThumbnail }}')
         "></div>
         <div class="absolute top-0 bottom-0 w-full bg-[rgba(14,16,21,0.5)]"></div>
         <div class="container mx-auto lg:relative lg:h-full">
-            <div class="relative px-4 sm:flex sm:items-center sm:px-0 lg:absolute lg:top-1/4 lg:left-10">
-                <img loading="lazy" class="rounded" src="{{ $game->card() }}" alt="">
+            <div class="relative p-4 sm:flex sm:items-center sm:p-0 lg:absolute lg:top-1/4 lg:left-10">
+                <img loading="lazy" class="rounded max-w-[256px] " src="{{ $game->card() }}" alt="">
                 <div class="mt-4 sm:ml-8 sm:mt-0 lg:ml-16">
-                    <h1 class="text-4xl font-extrabold leading-10 text-white">{{ $game->name }}</h1>
+                    <h1 class="mb-1 text-4xl font-extrabold leading-10 text-white">{{ $game->name }}</h1>
                     <p class="font-semibold text-white ">{{ $game->clips_count }} clips</p>
                 </div>
             </div>
