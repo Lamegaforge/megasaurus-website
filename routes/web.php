6757288663a631167fa3d0532c4655dfedba83a5
DeGraciaMathieu <degracia.mathieu@gmail.com>
2024-02-27 22:28:40 +0100

    small clip API

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Dtos\Uuid;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ShowClipController;
@@ -8,6 +9,7 @@ use App\Http\Controllers\PaginateClipController;
 use App\Http\Controllers\PaginateGameController;
 use App\Http\Controllers\ShowRandomClipController;
 use App\Http\Controllers\ToggleAutoplayController;
+use App\Repositories\FindDisplayableClipRepository;
 
 Route::get('/', HomeController::class)->name('home');
 
@@ -26,4 +28,24 @@ Route::prefix('games')
     ->group(function () {
         Route::get('/', PaginateGameController::class)->name('index');
         Route::get('{uuid}', ShowGameController::class)->name('show')->whereUuid('uuid');
+    });
+
+Route::prefix('api')
+    ->as('api.')
+    ->middleware([
+        'throttle:20,1',
+    ])
+    ->group(function () {
+        Route::get('clips/{uuid}', function (string $uuid) {
+
+            $clip = app(FindDisplayableClipRepository::class)->handle(
+                Uuid::fromString($uuid),
+            );
+
+            return response()->json([
+                'slug' => $clip->external_id,
+                'title' => $clip->title,
+                'game' => $clip->game->name,
+            ]);
+        });
     });
